9c8292c8ad2faf594cccf7a1126aa65779165228
XGH <ghxiong@incarcloud.com>
2017-04-21 23:12:00 +0800

    统计指标的模拟

--- src/main/resources/static/js/zdAPI.js
+++ src/main/resources/static/js/zdAPI.js
@@ -7,5 +7,27 @@
         return $.get('/api/getVehicleNum');
     }
 
+    // 模拟数据
+    zdAPI.prototype.fetchX1 = function () {
+        return Promise.resolve({
+            name: "累计行驶里程(km)",
+            good: 99999,
+            lo: 99,
+            hi: 99,
+            na: 99
+        });
+    }
+
+    // 模拟数据
+    zdAPI.prototype.fetchX2 = function () {
+        return Promise.resolve({
+            name: "满电续航里程(km)",
+            good: 99999,
+            lo: 99,
+            hi: 99,
+            na: 99
+        });
+    }
+
     return new zdAPI();
 });
--- src/main/resources/static/vcx/zd-stat.js
+++ src/main/resources/static/vcx/zd-stat.js
@@ -1,6 +1,42 @@
 ﻿define(['vue', 'text!./zd-stat.html', '../js/zdAPI'], function (Vue, template, zdAPI) {
+
+    var VisualModel = (function () {
+        function VisualModel() {
+            this.stat = [];
+            for (var i = 0; i < 6; i++) {
+                this.stat.push({
+                    name: "loading",
+                    good: "",
+                    lo: "",
+                    hi: "",
+                    na: ""
+                });
+            }
+        }
+
+        VisualModel.prototype.fetch = function () {
+            var _this = this;
+
+            zdAPI.fetchX1().then(function (value) {
+                Vue.set(_this.stat, 0, value);
+            });
+
+            zdAPI.fetchX2().then(function (value) {
+                Vue.set(_this.stat, 1, value);
+            });
+        };
+
+        return VisualModel;
+    })();
+
     var vcx = Vue.component('zd-stat', {
-        template
+        template,
+        data: function () {
+            var vm = new VisualModel();
+            vm.fetch();
+            
+            return vm;
+        }
     });
 
     return vcx;
